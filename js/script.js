dc7d6ab292f915f975378622fcf2cd85c2e38a25
purcelllj <purcelllj@gmail.com>
2015-09-08 00:12:31 -0700

    working on mobile responsiveness and navbar effects

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
             var searchBox = $('.searchbox');
             var isOpen = false;
             submitIcon.click(function(){
-                if(isOpen == false){
+                if(isOpen === false){
                     searchBox.addClass('searchbox-open');
                     inputBox.focus();
                     isOpen = true;
@@ -21,12 +21,48 @@
                     return false;
                 });
             $(document).mouseup(function(){
-                    if(isOpen == true){
+                    if(isOpen === true){
                         $('.searchbox-icon').css('display','block');
                         submitIcon.click();
                     }
                 });
+
+            
+            //change quote view in mobile
+            var $visibleDiv = $('#visible');
+            var $hiddenDiv = $('#hidden');
+
+           
+
+            if($(window).width() < 600){
+            	$visibleDiv.hide();
+            	$hiddenDiv.hide().fadeIn(2000);
+            }else{
+            	 $hiddenDiv.hide();
+            }
+
+
+            var scrollPosition = 0, delta = 50;
+
+            $(window).scroll(function(){
+                var scrollTop = $(this).scrollTop();
+                
+                if(Math.abs(scrollPosition - scrollTop) >= delta){
+                    if(scrollTop > scrollPosition){
+                        $('#navbar').css('opacity', 0.05);
+
+                    }else{
+                        $('#navbar').css('opacity', 1);
+                    }
+                    scrollPosition = scrollTop;
+                 }   
+            });
+
+
+
         });
+            
+
             function buttonUp(){
                 var inputVal = $('.searchbox-input').val();
                 inputVal = $.trim(inputVal).length;
