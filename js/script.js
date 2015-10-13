098c9ae7097b06d8cd1ce104cfaa063bafd1c8ab
purcelllj <purcelllj@gmail.com>
2015-10-13 02:08:21 -0700

    removed search, fixed mobile props, styled footer

--- js/script.js
+++ js/script.js
@@ -1,63 +1,63 @@
  $(document).ready(function(){
             
             //searchbox hide/show
-            var submitIcon = $('.searchbox-icon');
-            var inputBox = $('.searchbox-input');
-            var searchBox = $('.searchbox');
-            var isOpen = false;
-            submitIcon.click(function(){
-                if(isOpen === false){
-                    searchBox.addClass('searchbox-open');
-                    inputBox.focus();
-                    isOpen = true;
-                } else {
-                    searchBox.removeClass('searchbox-open');
-                    inputBox.focusout();
-                    isOpen = false;
-                }
-            });  
-             submitIcon.mouseup(function(){
-                    return false;
-                });
-            searchBox.mouseup(function(){
-                    return false;
-                });
-            $(document).mouseup(function(){
-                    if(isOpen === true){
-                        $('.searchbox-icon').css('display','block');
-                        submitIcon.click();
-                    }
-                });
+            // var submitIcon = $('.searchbox-icon');
+            // var inputBox = $('.searchbox-input');
+            // var searchBox = $('.searchbox');
+            // var isOpen = false;
+            // submitIcon.click(function(){
+            //     if(isOpen === false){
+            //         searchBox.addClass('searchbox-open');
+            //         inputBox.focus();
+            //         isOpen = true;
+            //     } else {
+            //         searchBox.removeClass('searchbox-open');
+            //         inputBox.focusout();
+            //         isOpen = false;
+            //     }
+            // });  
+            //  submitIcon.mouseup(function(){
+            //         return false;
+            //     });
+            // searchBox.mouseup(function(){
+            //         return false;
+            //     });
+            // $(document).mouseup(function(){
+            //         if(isOpen === true){
+            //             $('.searchbox-icon').css('display','block');
+            //             submitIcon.click();
+            //         }
+            //     });
 
             
             //pageload effects
-            var $visibleDiv = $('#visible');
-            var $hidden = $('#hidden');
+            // var $visibleDiv = $('#visible');
+            // var $hidden = $('#hidden');
 
-                if($(window).width() < 500){
-                    $hidden.hide();
-                    $visibleDiv.hide();
-                    $hidden.fadeIn('fast');
-                }else{
-                    $hidden.hide();
-                    $visibleDiv.hide().fadeIn('fast');
-                }
+            //     if($(window).width() < 500){
+            //         $hidden.hide();
+            //         $visibleDiv.hide();
+            //         $hidden.fadeIn('fast');
+            //     }else{
+            //         $hidden.hide();
+            //         $visibleDiv.hide().fadeIn('fast');
+            //     }
 
             //removing hidden search input in mobile view
 
-            var $dTop = $('#d-top');
-            var $mobile = $('#mobile');
+            // var $dTop = $('#d-top');
+            // var $mobile = $('#mobile');
 
 
-            if($(window).width() < 1070){
-                $('span#tely').hide();
-                $dTop.hide();
-                $mobile.show();
-            }else{
+            // if($(window).width() < 1070){
+            //     $('span#tely').hide();
+            //     $dTop.hide();
+            //     $mobile.show();
+            // }else{
                 
-                $mobile.hide();
-                $dTop.show();
-            }
+            //     $mobile.hide();
+            //     $dTop.show();
+            // }
 
 
             //navbar hide effect on scroll
@@ -78,24 +78,6 @@
             });
 
 
-            //testimonials dynamic view
-            
-            // var $execs = $('#executives');
-            // var $atts = $('#attorneys');
-            // var $des = $('#design');
-            // var $market = $('#marketing');
-            // var $listItem = $('.list-group-item');
-
-            // $execs.hide();
-            // $atts.hide();
-            // $des.hide();
-            // $market.hide();
-
-            // $listItem.on('click', function(){
-            //     $('article').hide();
-            //     $($(this).data('target')).show();
-
-            // });
         
             var $bout = $('#about-one-back'); 
 
@@ -123,38 +105,6 @@
             $contact.hide();
             $contact.fadeIn(1000);
 
-
-
-
-            //refresh page on resize
-            
-            var $width = $(window).width();
-
-
-            $(window).resize(function(){
-                if($(window).width() !== $width){
-                    location.reload();
-                }else{
-                    console.log('Window width has not changed. No refresh necessary')
-                    return false;
-                }
-            });
-
-
-            // $(window).resize(function(e){
-            //     e.preventDefault();
-            //     location.reload();
-
-            // });
-
-
-
-
-
-
-
-
-
         });
             
             
@@ -163,13 +113,4 @@
 
 
 
-            function buttonUp(){
-                var inputVal = $('.searchbox-input').val();
-                inputVal = $.trim(inputVal).length;
-                if( inputVal !== 0){
-                    $('.searchbox-icon').css('display','none');
-                } else {
-                    $('.searchbox-input').val('');
-                    $('.searchbox-icon').css('display','block');
-                }
-            }
+            
